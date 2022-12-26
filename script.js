29fef0b9cf89fa04afac06b8b8bf11187143e657
Mohieddine Farid <farid.mohieddine3@gmail.com>
2022-12-26 19:18:39 +0100

    basic javascript form added

--- script.js
+++ script.js
@@ -1,2 +1,60 @@
 'use strict'; // Enable strict mode
 
+const formInputs = [
+  "First Name",
+  "Last Name",
+  "Email",
+];
+const formSelects = [
+  "Study Format",
+  "Study Program",
+  "English Level",
+  "Study Location",
+  "Phone Number",
+  "Country"
+];
+
+const checks = document.querySelectorAll('.check');
+let inputs = document.querySelectorAll("input");
+
+let selects = document.querySelectorAll("select");
+
+let submitBtn = document.querySelector("#submit");
+function validateForm() {
+  // Make sure no text input is empty
+  for (let i = 0; i < formInputs.length; i++) {
+    if (inputs[i].value === "") {
+      alert(`${formInputs[i]} should not be empty!`);
+      return false;
+    }
+  }
+
+  // Make sure no select input is empty
+  for (let j = 0; j < formSelects.length; j++) {
+    if (selects[i].value === "") {
+      alert(`${selects[j].value} should not be empty!`);
+      return false
+    }
+    else {
+      alert(`${selects[j].value} are not empty!`);
+    }
+    alert("Everything is fine, the form will be submitted !");
+    return true;
+  }
+}
+
+for (let i = 0; i < selects.length; i++) {
+  selects[i].addEventListener('change', function() {
+    if (selects[i].value !== '') {
+      checks[i].style.display = "inline-block";
+    } else {
+      checks[i].style.display = "none";
+    }
+  }, false);
+}
+
+document.querySelector("form").addEventListener("submit", function(event) {
+  if (!validateForm()) {
+    event.preventDefault();
+  }
+});
